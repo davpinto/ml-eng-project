0df8b9e0d4b0488c25b46f912192963fd5ceff51
David Pinto <david.pinto@sympla.com.br>
2020-10-20 22:22:36 -0300

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
-echo "
-[server]\n
-headless = true\n
-enableCORS=false\n
-port = $PORT\n
-\n
+
+echo "\
+[general]\n\
+email = \"davidengaut@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
